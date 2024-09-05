25c028a26e50576f3d104fea26dce3123e017366
andersonalves45 <anderson.oliveira@livsaude.com.br>
2024-09-05 20:49:31 -0300

    feat: create test

--- src/modules/authenticate/services/authenticate/authenticate.spec.ts
+++ src/modules/authenticate/services/authenticate/authenticate.spec.ts
@@ -2,9 +2,10 @@ import bcryptjs from "bcryptjs";
 import { InMemoryUserRepository } from "@/modules/user/repositories/in-memory/in-memory-user.repository";
 import { describe, expect, it } from "vitest";
 import { AuthenticateService } from "./authenticate.service";
+import { InvalidCredentialsError } from "../errors/invalid-credentials-error";
 
 describe("Authenticate Service", () => {
-  it("should not be able to register with same email", async () => {
+  it("should be able authenticate", async () => {
     const usersRepository = new InMemoryUserRepository();
     const sut = new AuthenticateService(usersRepository);
 
@@ -22,5 +23,37 @@ describe("Authenticate Service", () => {
     });
 
     expect(user.id).toEqual(expect.any(String));
+  });
+
+  it("should not be able to authenticate with wrong username", async () => {
+    const usersRepository = new InMemoryUserRepository();
+    const sut = new AuthenticateService(usersRepository);
+
+    expect(() =>
+      sut.execute({
+        username: "teste",
+        password: "teste@123",
+      })
+    ).rejects.toBeInstanceOf(InvalidCredentialsError);
+  });
+
+  it("should not be able to authenticate with wrong password", async () => {
+    const usersRepository = new InMemoryUserRepository();
+    const sut = new AuthenticateService(usersRepository);
+
+    await usersRepository.create({
+      name: "Anderson",
+      email: "anderson@test.com",
+      password: await bcryptjs.hash("teste@1234", 6),
+      perfil: "A",
+      username: "anderson",
+    });
+
+    expect(() =>
+      sut.execute({
+        username: "anderson",
+        password: "teste@1",
+      })
+    ).rejects.toBeInstanceOf(InvalidCredentialsError);
   });
 });
