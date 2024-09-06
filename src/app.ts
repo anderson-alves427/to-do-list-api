82a83ef98d217c28e11b294de751b2a58fbf07e2
andersonalves45 <anderson.oliveira@livsaude.com.br>
2024-09-06 05:57:53 -0300

    fix: some valitions with user

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,15 @@ import { ZodError } from "zod";
 import { env } from "./env";
 import { appRoutes } from "./modules/routes/app.routes";
 import fastifyJwt from "@fastify/jwt";
+import fastifyCors from "@fastify/cors";
 
 export const app = fastify();
+
+app.register(fastifyCors, {
+  origin: true, // Permite qualquer origem
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"], // Métodos permitidos
+  credentials: true, // Se você quiser habilitar o envio de cookies ou credenciais
+});
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
@@ -15,7 +22,7 @@ app.register(appRoutes);
 app.setErrorHandler((error, _request, reply) => {
   if (error instanceof ZodError) {
     return reply.status(400).send({
-      message: "Erro dna validação de dados.",
+      message: "Erro na validação de dados.",
       issues: error.format(),
     });
   }
